1eb25ffc83d66ecd6228b3b0287f55d3f59c7e24
Patrick Janssen <akiphtj@nus.edu.sg>
2017-11-02 10:15:28 +0800

    added src

---
+++ src/modules/polygons.ts
@@ -0,0 +1,9 @@
+import * as ent from "../libs/gs_json/utils/entities";
+
+export class Pgon {
+	public makePolygon(points:ent.IPoint[]):ent.IPolygon {
+		//to be implemented, this is just a temporary example
+		return {points:points};
+	}
+	//lots more to come here
+}
