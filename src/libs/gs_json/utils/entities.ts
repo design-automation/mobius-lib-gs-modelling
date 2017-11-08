4a6eaf121bf3249745d4f2549687b69f9f95548e
Mastertype <33281683+Mastertype@users.noreply.github.com>
2017-11-08 17:26:53 +0800

    Update entities.ts

--- src/libs/gs_json/utils/entities.ts
+++ src/libs/gs_json/utils/entities.ts
@@ -9,3 +9,26 @@ export interface IPolyline {
 export interface IPolygon {
 	points: IPoint[];
 }
+export interface IVect{
+    a: IPoint;
+    b: IPoint;
+}
+export interface IVec {
+    a: IPoint;
+    b: IPoint;
+}
+export interface IExtrusion{
+    Pgon:IPolygon;
+    Axis:IVec;
+}
+export interface IRevolve{
+    Pgon:IPolygon;
+    Axis:IVec;
+}
+export interface ILoft{
+    Pgons:IPolygon[];
+}
+export interface ISweap{
+    Pgon:IPolygon;
+    Pline:IPolyline;
+}
