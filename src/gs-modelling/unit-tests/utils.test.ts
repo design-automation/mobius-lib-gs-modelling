fb80b5a53f95b97d657c936d61ad9cf52e37c769
Kamel Adouane <sdeka@nus.edu.sg>
2017-12-21 11:37:11 +0800

    fixed bu

--- src/gs-modelling/unit-tests/utils.test.ts
+++ src/gs-modelling/unit-tests/utils.test.ts
@@ -3,7 +3,7 @@ import * as tests from "../lib/utils_tests";
 
 // an eample of a simple test
 describe("Utils tests", () => {
-    it("test_MoveObject", () => {
+    it("test_transfromXYZ", () => {
         expect( tests.test_transfromXYZ() ).toBe(true);
     });
 });
