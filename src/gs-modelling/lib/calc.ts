5c6a253ddfd5d0f3da8e48558ff561ccd0df73c2
Kamel Adouane <sdeka@nus.edu.sg>
2017-12-26 17:04:10 +0800

    Quartics, P,R,d0,D

--- src/gs-modelling/lib/calc.ts
+++ src/gs-modelling/lib/calc.ts
@@ -19,5 +19,26 @@ export function plineLength(m: gs.IModel, pline: gs.IPolyline, segment_index: nu
  * @returns the four roots
  */
 export function quarticRoots(coefficients: number[]): number[] {
-    throw new Error("Method not implemented");
- }
+    if(!(coefficients.length === 5)) {throw new Error("5 coefficients required for a quartic");}
+    const a: number = coefficients[0];
+    const b: number = coefficients[1];
+    const c: number = coefficients[2];
+    const d: number = coefficients[3];
+    const e: number = coefficients[4];
+
+    const delta: number =  256*Math.pow(a,3) - 192*Math.pow(a,2)*b*d*Math.pow(e,2)
+                         - 128*Math.pow(a,2)*Math.pow(c,2)*Math.pow(e,2) + 144*Math.pow(a,2)*c*Math.pow(d,2)*e
+                         - 27*Math.pow(a,2)*Math.pow(d,4) + 144*a*Math.pow(b,2)*c*Math.pow(e,2)
+                         - 6*a*Math.pow(b,2)*Math.pow(d,2)*e - 80*a*b*Math.pow(c,2)*d*e + 18*a*b*c*Math.pow(d,3)
+                         + 16*a*Math.pow(c,4)*e - 4*a*Math.pow(c,3)*Math.pow(d,2) - 27*Math.pow(b,4)*Math.pow(e,2)
+                         + 18*Math.pow(b,3)*c*d*e - 4*Math.pow(b,3)*Math.pow(d,3) - 4*Math.pow(b,3)*Math.pow(d,3)
+                         - 4*Math.pow(b,2)*Math.pow(c,3)*e + Math.pow(b*c*d,2);
+
+    const P: number = 8*a*c - 3*Math.pow(b,2);
+    const R: number = Math.pow(b,3) + 8*d*Math.pow(a,2);
+    const delta_0: number = Math.pow(c,2) - 3*b*d + 12*a*e;
+    const D: number =  64*Math.pow(a,3)*e - 16* Math.pow(a,2)*Math.pow(c,2) + 16*a*Math.pow(b,2)*c
+                     - 16*Math.pow(a,2)*b*d - 3*Math.pow(b,4);
+
+    return [];
+     }
