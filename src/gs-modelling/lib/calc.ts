bb9536a87efaf216146f40a647bd346dd31f0cc1
Lim Yan Yee Joie <a0115775@nus.edu.sg>
2017-12-27 10:08:04 +0800

    Doccumentation based on API

--- src/gs-modelling/lib/calc.ts
+++ src/gs-modelling/lib/calc.ts
@@ -1,6 +1,54 @@
 import * as gs from "gs-json";
 
 /**
+ * Calculates distance between two points or two clusters of points
+ * @param points_1 Point 1 or first cluster of points
+ * @param points_2 Point 2 or second cluster of points
+ * @param min Returns minimum distance between two clusters of points if true, maximum distance if false
+ * @returns Distance between points if successful, none if unsuccessful or on error
+ */
+export function distBetweenPoints(point_1: gs.IPoint, point_2: gs.IPoint, min: boolean=true ) {
+    throw new Error("Method not implemented");
+}
+
+/**
+ * Returns the roots of a four degree polynomial
+ * @param the five coefficients of the polynomial
+ * @returns the four roots
+ */
+export function quarticRoots(coefficients: number[]): number[] {
+    if(!(coefficients.length === 5)) {throw new Error("5 coefficients required for a quartic");}
+    const a: number = coefficients[0];
+    const b: number = coefficients[1];
+    const c: number = coefficients[2];
+    const d: number = coefficients[3];
+    const e: number = coefficients[4];
+    const p: number = (8*a*c - 3*Math.pow(b,2))/(8*Math.pow(a,2));
+    const q: number = (Math.pow(b,3) - 4*a*b*c + 8*Math.pow(a,2)*d)/(8*Math.pow(a,3));
+    if(a === 0) {throw new Error("Non quartic polynomial");}
+    const delta: number =  256*Math.pow(a,3) - 192*Math.pow(a,2)*b*d*Math.pow(e,2)
+                         - 128*Math.pow(a,2)*Math.pow(c,2)*Math.pow(e,2) + 144*Math.pow(a,2)*c*Math.pow(d,2)*e
+                         - 27*Math.pow(a,2)*Math.pow(d,4) + 144*a*Math.pow(b,2)*c*Math.pow(e,2)
+                         - 6*a*Math.pow(b,2)*Math.pow(d,2)*e - 80*a*b*Math.pow(c,2)*d*e + 18*a*b*c*Math.pow(d,3)
+                         + 16*a*Math.pow(c,4)*e - 4*a*Math.pow(c,3)*Math.pow(d,2) - 27*Math.pow(b,4)*Math.pow(e,2)
+                         + 18*Math.pow(b,3)*c*d*e - 4*Math.pow(b,3)*Math.pow(d,3) - 4*Math.pow(b,3)*Math.pow(d,3)
+                         - 4*Math.pow(b,2)*Math.pow(c,3)*e + Math.pow(b*c*d,2);
+
+    const P: number = 8*a*c - 3*Math.pow(b,2);
+    const R: number = Math.pow(b,3) + 8*d*Math.pow(a,2);
+    const delta_0: number = Math.pow(c,2) - 3*b*d + 12*a*e;
+    const delta_1: number = 2*Math.pow(c,3) - 9*b*c*d + 27*Math.pow(b,2)*e +27*a*Math.pow(d,2) - 72*a*c*e;
+    const D: number =  64*Math.pow(a,3)*e - 16* Math.pow(a,2)*Math.pow(c,2) + 16*a*Math.pow(b,2)*c
+                     - 16*Math.pow(a,2)*b*d - 3*Math.pow(b,4);
+
+    // const S: number = Math.pow();
+    // const Q: number = Math.pow();
+    return [];
+     }
+
+////////////////////////////////////////old functions no longer in API list///////////////////////////////////////////////
+
+/**
  * Returns length of a polyline object
  * @param m Model
  * @param polyline Polyline object.
