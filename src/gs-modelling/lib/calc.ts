ac62878f910cf33126cee8ebcdcc1ba32c82102d
Kamel Adouane <sdeka@nus.edu.sg>
2018-01-03 19:56:29 +0800

    distance between points

--- src/gs-modelling/lib/calc.ts
+++ src/gs-modelling/lib/calc.ts
@@ -10,8 +10,22 @@ import * as quadratic from "solve-quadratic-equation";
  * @param min Returns minimum distance between two clusters of points if true, maximum distance if false
  * @returns Distance between points if successful, none if unsuccessful or on error
  */
-export function distBetweenPoints(point_1: gs.IPoint, point_2: gs.IPoint, min: boolean=true ) {
-    throw new Error("Method not implemented");
+export function distBetweenPoints(point_1: gs.IPoint[], point_2: gs.IPoint[], minimum: boolean=true ) {
+        let min: number = 0;
+        let max: number = 0;
+        let distance: number = 0;
+        for(p1 of point_1) {
+            for(p2 of point_2) {
+                        distance = Math.sqrt(
+                        (p1.getPosition()[0] - p2.getPosition()[0])*(p1.getPosition()[0] - p2.getPosition()[0])
+                       + (p1.getPosition()[1] - p2.getPosition()[1])*(p1.getPosition()[1] - p2.getPosition()[1])
+                       + (p1.getPosition()[2] - p2.getPosition()[2])*(p1.getPosition()[2] - p2.getPosition()[2]) );
+                        if( distance > max) { max = distance;}
+                        if( distance < min) { min = distance;}
+            }
+        }
+        if( minimum === true) {return min;}
+        return max;
 }
 
 export function identifier(coeff: number[]): number[] {
