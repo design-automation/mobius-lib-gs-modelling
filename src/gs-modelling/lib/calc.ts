165138e96f45ca6eefebe19d7a686e3c4b366d24
Kamel Adouane <sdeka@nus.edu.sg>
2018-01-04 11:10:29 +0800

    fixed bug

--- src/gs-modelling/lib/calc.ts
+++ src/gs-modelling/lib/calc.ts
@@ -14,8 +14,8 @@ export function distBetweenPoints(point_1: gs.IPoint[], point_2: gs.IPoint[], mi
         let min: number = 0;
         let max: number = 0;
         let distance: number = 0;
-        for(p1 of point_1) {
-            for(p2 of point_2) {
+        for(const p1 of point_1) {
+            for(const p2 of point_2) {
                         distance = Math.sqrt(
                         (p1.getPosition()[0] - p2.getPosition()[0])*(p1.getPosition()[0] - p2.getPosition()[0])
                        + (p1.getPosition()[1] - p2.getPosition()[1])*(p1.getPosition()[1] - p2.getPosition()[1])
