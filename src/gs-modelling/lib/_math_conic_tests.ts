05a78def2f30fdf2de24aff4d85a4afbf7c0b516
Kamel Adouane <sdeka@nus.edu.sg>
2018-01-12 11:23:13 +0800

    Unit Testing FrameWork for Math Conic Functions

---
+++ src/gs-modelling/lib/_math_conic_tests.ts
@@ -0,0 +1,51 @@
+import * as gs from "gs-json";
+import * as test from "./_math_conic_dev";
+
+export function test_isectCircleCircle2D() {
+// _isectCircleCircle2D(circle1: gs.ICircle, circle2: gs.ICircle): gs.IPoint[] {
+    return true;
+}
+export function test_isectCircleEllipse2D() {
+// _isectCircleEllipse2D(circle: gs.ICircle, ellipse: gs.IEllipse): gs.IPoint[]
+    return true;
+}
+export function test_isectEllipseEllipse2D() {
+// _isectEllipseEllipse2D(ellipse1: gs.IEllipse, ellipse2: gs.IEllipse): gs.IPoint[]
+    return true ;
+}
+export function test_distBetweenPoints() {
+    // distBetweenPoints(point_1: gs.IPoint[], point_2: gs.IPoint[], minimum: boolean=true )
+    return true ;
+}
+export function test_identifier() {
+// identifier(coeff: number[]): number[]
+    return true;
+}
+export function test_General_Form() {
+// General_Form(conic1: number[], origin1: number[], origin2: number[], alpha: number): number[]
+    return true;
+}
+export function test_Split() {
+// Split(conic1: number[], conic2: number[], origin1: number[], origin2: number[], alpha: number): number[][]
+    return true;
+}
+export function test_Function() {
+// Function_F(x: number): number
+    return true;
+}
+export function test_parabola_lenght() {
+// parabola_lenght(conic: number[] , x1: number, x2: number): number
+    return true;
+}
+export function test_ellipse_length() {
+// ellipse_length(conic: number[], theta_1: number, theta_2: number): number
+    return true;
+}
+export function test_hyperbola_length() {
+// hyperbola_length(conic: number[], theta_1: number, theta_2: number): number
+    return true;
+}
+export function test_plineLength() {
+// plineLength(m: gs.IModel, pline: gs.IPolyline, segment_index: number, sub_domain: [number,number] ): number
+    return true;
+}
