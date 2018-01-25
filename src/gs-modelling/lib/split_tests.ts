cc6b0fe32b6cfa1b281b7ff31eb896f7cb1ced81
Kamel Adouane <sdeka@nus.edu.sg>
2018-01-25 11:39:35 +0800

    Unit TEst Split

---
+++ src/gs-modelling/lib/split_tests.ts
@@ -0,0 +1,23 @@
+import * as gs from "gs-json";
+import * as sl from "./split";
+
+export function test_circleCircle2D(): boolean {
+    const m: gs.IModel = new gs.Model();
+    const g: gs.IGeom = m.getGeom();
+    const pt1: gs.IPoint = g.addPoint([-0.5,0,0]);
+    const pt2: gs.IPoint = g.addPoint([ 0.5,0,0]);
+    const radius: number = 1;
+    const circle1: gs.ICircle = g.addCircle(pt1, [1,0,0],[0,0,1], [0,360]);
+    const circle2: gs.ICircle = g.addCircle(pt2, [1,0,0],[0,0,1], [0,360]);
+    const Arcs: gs.ICircle[] = sl.circleCircle2D(circle1,circle2);
+    let k: number = 1;
+    for (const arc of Arcs) {
+        console.log("circle " + k);
+        console.log("    Radius " + arc.getRadius());
+        console.log("    Angles " + arc.getAngles()[0] + "    " + arc.getAngles()[1]);
+        console.log("    Vectors_x    " + arc.getVectors()[0][0] + "    "+ arc.getVectors()[0][1] + "    "+ arc.getVectors()[0][2]);
+        console.log("    Vectors_y    " + arc.getVectors()[1][0] + "    "+ arc.getVectors()[1][1] + "    "+ arc.getVectors()[1][2]);
+        k++;
+    }
+    return true;
+}
