f56575356acfb188f509c3127c14677614808502
Kamel Adouane <sdeka@nus.edu.sg>
2018-01-25 13:16:40 +0800

    fixed bugs

--- src/gs-modelling/lib/split.ts
+++ src/gs-modelling/lib/split.ts
@@ -39,34 +39,42 @@ export function circleCircle2D(circle1: gs.ICircle, circle2: gs.ICircle): gs.ICi
     if (!circle2.exists()) {throw new Error("circle2 has been deleted.");}
     const model: gs.IModel = circle1.getModel();
     const geom: gs.IGeom = model.getGeom();
-    const points: gs.IPoint[] = math_conic._isectCircleCircle2D(circle1, circle2);
+    let points: gs.IPoint[] = math_conic._isectCircleCircle2D(circle1, circle2);
     if (points.length !== 2) {return null;}
     const circle1_origin: gs.IPoint = circle1.getOrigin();
     const circle2_origin: gs.IPoint = circle2.getOrigin();
+    const order_pt0: three.Vector3 = threex.vectorFromPointsAtoB(circle1_origin, points[0]);
+    const order_pt1: three.Vector3 = threex.vectorFromPointsAtoB(circle1_origin, points[1]);
+    const angle_check: number = order_pt0.angleTo(order_pt1)*180/Math.PI;
+    if(angle_check > 180) {points = [points[1],points[0]];}
     //arc 1 a
-    const vec1_x: three.Vector3 = threex.vectorFromPointsAtoB(circle1_origin, points[0]);
-    const vec1_2nd_x: three.Vector3 = threex.vectorFromPointsAtoB(circle1_origin, points[1]);
-    const vec1_y: three.Vector3 = threex.orthoVectors(vec1_x, vec1_2nd_x);
+    const vec1_x: three.Vector3 = threex.vectorFromPointsAtoB(circle1_origin, points[1]);
+    const vec1_2nd_x: three.Vector3 = threex.vectorFromPointsAtoB(circle1_origin, points[0]);
+    const vec1_y: three.Vector3 = (threex.orthoVectors(vec1_x, vec1_2nd_x).normalize()).multiplyScalar(vec1_x.length());
     const angle1: number = vec1_x.angleTo(vec1_2nd_x);
     const vec1_x_xyz: gs.XYZ = vec1_x.toArray() as gs.XYZ;
     const vec1_y_xyz: gs.XYZ = vec1_y.toArray() as gs.XYZ;
-    const arc1_a: gs.ICircle = geom.addCircle(circle1_origin, vec1_x_xyz, vec1_y_xyz, [0, angle1]);
+    const arc1_a: gs.ICircle = geom.addCircle(circle1_origin, vec1_x_xyz, vec1_y_xyz, [0, angle1*180/Math.PI]);
     // arc 1 b
     const vec1_2nd_x_xyz: gs.XYZ = vec1_2nd_x.toArray() as gs.XYZ;
-    const vec1_2nd_y_xyz: gs.XYZ = threex.orthoVectors(vec1_x, vec1_2nd_x).toArray() as gs.XYZ;
-    const arc1_b: gs.ICircle = geom.addCircle(circle1_origin, vec1_2nd_x_xyz, vec1_2nd_y_xyz, [0, 360 - angle1]);
+    const vec1_2nd_y_xyz: gs.XYZ = threex.orthoVectors(
+        vec1_2nd_x,vec1_x).normalize().multiplyScalar(-vec1_2nd_x.length()).toArray() as gs.XYZ;
+    const arc1_b: gs.ICircle = geom.addCircle(
+        circle1_origin, vec1_2nd_x_xyz, vec1_2nd_y_xyz, [0, 360 - angle1*180/Math.PI]);
     //arc 2 a
     const vec2_x: three.Vector3 = threex.vectorFromPointsAtoB(circle2_origin, points[0]);
     const vec2_2nd_x: three.Vector3 = threex.vectorFromPointsAtoB(circle2_origin, points[1]);
-    const vec2_y: three.Vector3 = threex.orthoVectors(vec2_x, vec2_2nd_x);
-    const angle2: number = vec1_x.angleTo(vec2_2nd_x);
+    const vec2_y: three.Vector3 = (threex.orthoVectors(vec2_x, vec2_2nd_x).normalize()).multiplyScalar(vec2_x.length());
+    const angle2: number = vec2_x.angleTo(vec2_2nd_x);
     const vec2_x_xyz: gs.XYZ = vec2_x.toArray() as gs.XYZ;
     const vec2_y_xyz: gs.XYZ = vec2_y.toArray() as gs.XYZ;
-    const arc2_a: gs.ICircle = geom.addCircle(circle2_origin, vec2_x_xyz, vec2_y_xyz, [0, angle2]);
+    const arc2_a: gs.ICircle = geom.addCircle(circle2_origin, vec2_x_xyz, vec2_y_xyz, [0, angle2*180/Math.PI]);
     // arc 2 b
     const vec2_2nd_x_xyz: gs.XYZ = vec2_2nd_x.toArray() as gs.XYZ;
-    const vec2_2nd_y_xyz: gs.XYZ = threex.orthoVectors(vec2_x, vec2_2nd_x).toArray() as gs.XYZ;
-    const arc2_b: gs.ICircle = geom.addCircle(circle2_origin, vec2_2nd_x_xyz, vec2_2nd_y_xyz, [0, 360 - angle2]);
+    const vec2_2nd_y_xyz: gs.XYZ = (threex.orthoVectors(
+        vec2_2nd_x, vec2_x).normalize()).multiplyScalar(-vec2_x.length()).toArray() as gs.XYZ;
+    const arc2_b: gs.ICircle = geom.addCircle(
+        circle2_origin, vec2_2nd_x_xyz, vec2_2nd_y_xyz, [0, 360 - angle2*180/Math.PI]);
     // return arcs
     return [arc1_b, arc1_a, arc2_a, arc2_b];
 }
--- src/gs-modelling/lib/split_tests.ts
+++ src/gs-modelling/lib/split_tests.ts
@@ -9,14 +9,14 @@ export function test_circleCircle2D(): boolean {
     const radius: number = 1;
     const circle1: gs.ICircle = g.addCircle(pt1, [1,0,0],[0,0,1], [0,360]);
     const circle2: gs.ICircle = g.addCircle(pt2, [1,0,0],[0,0,1], [0,360]);
-    const Arcs: gs.ICircle[] = sl.circleCircle2D(circle1,circle2);
+    const arcs: gs.ICircle[] = sl.circleCircle2D(circle1,circle2);
     let k: number = 1;
-    for (const arc of Arcs) {
+    for (const arc of arcs) {
         console.log("circle " + k);
         console.log("    Radius " + arc.getRadius());
-        console.log("    Angles " + arc.getAngles()[0] + "    " + arc.getAngles()[1]);
-        console.log("    Vectors_x    " + arc.getVectors()[0][0] + "    "+ arc.getVectors()[0][1] + "    "+ arc.getVectors()[0][2]);
-        console.log("    Vectors_y    " + arc.getVectors()[1][0] + "    "+ arc.getVectors()[1][1] + "    "+ arc.getVectors()[1][2]);
+        console.log("    Angles [" + arc.getAngles()[0] + "  to  " + arc.getAngles()[1] + "]");
+        console.log("    Vectors_x    [" + arc.getVectors()[0][0] + "  ,  "+ arc.getVectors()[0][1] + "  ,  "+ arc.getVectors()[0][2] + "]");
+        console.log("    Vectors_y    [" + arc.getVectors()[1] + "]");
         k++;
     }
     return true;
